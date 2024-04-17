48312f31610879bf8f39821ccc4dc5c9cfd0c504
Abubakre Bamisebi <abuolabamisebi@gmail.com>
2024-04-17 22:27:05 +0100

    first commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var text = document.getElementById("toggleText");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
